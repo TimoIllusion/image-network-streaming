c293e94844410af957e3cf5c047ae802ad3f2dea
Timo Leitritz <42964574+TimoIllusion@users.noreply.github.com>
2025-03-21 17:57:47 +0100

    Delete duplicated files

--- test_media.py
+++
@@ -1,8 +0,0 @@
-from image_network_streaming.frontend import media
-
-
-def test_media():
-
-    frame = next(media.generate_frames())
-
-    assert type(frame) is bytes, "Camera could not generate frames."
